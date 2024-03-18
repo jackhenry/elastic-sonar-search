41728dc28b9169e4143cb200683fa4eef11ea71b
Jack Henry <jack@jackerickson.net>
2024-03-18 13:45:46 -0700

    Changed dotenv to dotenv-cli. Importing dotenv is funky when building to ESM.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,6 @@
 import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import { searchRouter } from "./trpc/trpc";
 import cors from "cors";
-import "dotenv/config";
 
 const server = createHTTPServer({
   middleware: cors(),
